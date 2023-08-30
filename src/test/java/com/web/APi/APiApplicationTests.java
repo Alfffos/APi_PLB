c3857e21f31c75b29df01d583bebf524db59938f
Alfffos <alfonsogaray03@gmail.com>
2023-08-30 09:42:34 -0300

    first commit

---
+++ src/test/java/com/web/APi/APiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.APi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
