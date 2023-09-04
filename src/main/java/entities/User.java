afcf8b7be4b56967169b0514fd614bdddf6eb84a
alfonso <alfonsogaray03@gmail.com>
2023-09-04 16:29:49 -0300

    firsth branch

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,2 +1,80 @@
-package entities;public class user {
+package entities;
+import jakarta.persistence.*;
+@Entity
+@Table
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Integer userId;
+    @Column
+    private String userName;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Rol role;                        //falta crear la entidad Rol para usarlo como tipo de dato.
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Rol getRole() {
+        return role;
+    }
+
+    public void setRole(Rol role) {
+        this.role = role;
+    }
 }
